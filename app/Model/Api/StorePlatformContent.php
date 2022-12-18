bd138a96040ac2a2851d587dc144a31a59cfa393
7small7 <1005349393@qq.com>
2022-12-18 22:32:50 +0800

    修改路由地址

--- app/Model/Api/StorePlatformContent.php
+++ app/Model/Api/StorePlatformContent.php
@@ -1,18 +1,17 @@
 <?php
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace App\Model\Api;
 
 /**
  * 平台内容
- *
  * Class StorePlatformContent
  * @package App\Model\Api
  */
 class StorePlatformContent extends \App\Model\Common\StorePlatformContent
 {
-	public $searchFields = [
-		'content',
-		'title',
-	];
+    public $searchFields = [
+        'content',
+        'title',
+    ];
 }
