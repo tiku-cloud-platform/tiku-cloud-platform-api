0155eb632f8dd6240500be0b8feab21e440e7583
7small7 <1005349393@qq.com>
2023-01-17 03:48:35 +0800

    教程查询验证

--- app/Model/Api/StoreBook.php
+++ app/Model/Api/StoreBook.php
@@ -11,16 +11,4 @@ class StoreBook extends \App\Model\Common\StoreBook
     protected $hidden = [
         "file_uuid",
     ];
-
-    protected $appends = [
-        "read_number",
-    ];
-
-    public function getReadNumberAttribute(): int
-    {
-        return (int)(new StoreBookContent())::query()->where([
-            ["store_book_uuid", "=", $this->getAttribute("uuid")]
-        ])->sum("read_number");
-
-    }
 }
