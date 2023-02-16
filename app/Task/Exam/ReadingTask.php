3885ad0473e336b4d0c8ec8c2c63a005252e5b2b
7small7 <1005349393@qq.com>
2023-02-16 18:20:43 +0800

    积分检测

--- app/Task/Exam/ReadingTask.php
+++ app/Task/Exam/ReadingTask.php
@@ -10,6 +10,7 @@ use App\Model\Shell\StoreUserScoreCollection;
 use App\Model\Shell\StoreUserScoreHistory;
 use Hyperf\Crontab\Annotation\Crontab;
 use Hyperf\DbConnection\Db;
+use Throwable;
 
 /**
  * @Crontab(name="reading_task", rule="* * * * * *" , callback="execute", memo="用户问答试题积分")
@@ -22,36 +23,40 @@ class ReadingTask
         $value = RedisClient::getInstance()->lPop(CacheKey::EXAM_READING);
         if (!empty($value)) {
             $value = json_decode($value, true);
-            $row   = 0;
-            Db::transaction(function () use ($value, &$row) {
-                $scoreModel = new StoreUserScoreHistory();
-                $scoreModel::query()->create([
-                    "client_type" => $value["client_type"],
-                    "uuid" => UUID::getUUID(),
-                    "title" => "答题积分",
-                    "type" => 1,
-                    "score" => $value["income_score"],
-                    "user_uuid" => $value["user_uuid"],
-                    "store_uuid" => $value["store_uuid"],
-                ]);
-                $scoreModel::query()->create([
-                    "client_type" => $value["client_type"],
-                    "uuid" => UUID::getUUID(),
-                    "title" => "答题积分",
-                    "type" => 2,
-                    "score" => $value["expend_score"],
-                    "user_uuid" => $value["user_uuid"],
-                    "store_uuid" => $value["store_uuid"],
-                ]);
+            Db::beginTransaction();
+            $scoreModel = new StoreUserScoreHistory();
+            try {
+                if (!empty($value["income_score"])) {
+                    $scoreModel::query()->create([
+                        "client_type" => $value["client_type"],
+                        "uuid" => UUID::getUUID(),
+                        "title" => "答题积分",
+                        "type" => 1,
+                        "score" => $value["income_score"],
+                        "user_uuid" => $value["user_uuid"],
+                        "store_uuid" => $value["store_uuid"],
+                    ]);
+                }
+                if (!empty($value["expend_score"])) {
+                    $scoreModel::query()->create([
+                        "client_type" => $value["client_type"],
+                        "uuid" => UUID::getUUID(),
+                        "title" => "答题积分",
+                        "type" => 2,
+                        "score" => $value["expend_score"],
+                        "user_uuid" => $value["user_uuid"],
+                        "store_uuid" => $value["store_uuid"],
+                    ]);
+                }
                 RedisClient::getInstance()->incrByFloat(CacheKey::SCORE_TOTAL . $value["user_uuid"], $value["income_score"] - $value["expend_score"]);
                 (new StoreUserScoreCollection())::query()->where([
                     ["user_uuid", "=", $value["user_uuid"]],
                     ["store_uuid", "=", $value["store_uuid"]],
                 ])->increment("score", $value["income_score"] - $value["expend_score"]);
-                $row = 1;
-            }, 2);
-            if ($row < 1) {
+                Db::commit();
+            } catch (Throwable $throwable) {
                 RedisClient::getInstance()->lPush(CacheKey::EXAM_READING, json_encode($value, JSON_UNESCAPED_UNICODE));
+                Db::rollBack();
             }
         }
     }
