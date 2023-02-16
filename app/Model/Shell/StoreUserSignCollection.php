967f149a0d0851fe60d9dfea7ed565bfaf8c31cb
7small7 <1005349393@qq.com>
2023-02-16 10:34:15 +0800

    签到队列

---
+++ app/Model/Shell/StoreUserSignCollection.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Model\Shell;
+
+use App\Model\Model;
+
+class StoreUserSignCollection extends Model
+{
+    protected $table = "store_user_sign_collection";
+}
