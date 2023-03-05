d421cea403f80dec5618e57088719dca6bce5533
7small7 <1005349393@qq.com>
2023-03-05 19:22:02 +0800

    添加字段

--- config/autoload/exceptions.php
+++ config/autoload/exceptions.php
@@ -2,6 +2,7 @@
 declare(strict_types = 1);
 
 use App\Exception\Handler\DbDataMessageExceptionHandler;
+use App\Exception\Handler\DbDuplicateMessageExceptionHandler;
 use App\Exception\Handler\FromValidateExceptionHandler;
 use App\Exception\Handler\MethodNotAllowedHttpExceptionHandler;
 use App\Exception\Handler\NotFundHttpExceptionHandler;
@@ -11,6 +12,7 @@ return [
     'handler' => [
         'http' => [
             ScoreExceptionHandler::class,
+            DbDuplicateMessageExceptionHandler::class,
             DbDataMessageExceptionHandler::class,
             FromValidateExceptionHandler::class,
             NotFundHttpExceptionHandler::class,
